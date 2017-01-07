501558caff14013860704aa91220eed81d6204bc
MuthuA <kuttymuthu55@gmail.com>
2017-01-07 00:32:41 -0800

    Create leaf.c

---
+++ leaf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ int year;
+ printf("enter year");
+ scanf("%d",&year);
+ if(year %4==0)
+   printf("leap year");
+   else
+    printf("not leaf year");
+    return 0;
+   }
