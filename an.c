5e8914765040ac73c5763f974407e82c694a7051
MuthuA <kuttymuthu55@gmail.com>
2017-01-07 00:20:37 -0800

    Create an.c

---
+++ an.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ chat str;
+ printf("enter character");
+ scanf("%c",&str);
+ if(str>=65 && str<=90)
+  printf("alphabet");
+  else
+  printf("not alphabet");
+  return 0;
+  }
