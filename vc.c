ee212e74d322dff197c39dc8cd5fa720922a7884
MuthuA <kuttymuthu55@gmail.com>
2017-01-07 00:16:08 -0800

    Create vc.c

---
+++ vc.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+char str;
+printf("enter character");
+scanf("%c"&str);
+if(str=='a' || str=='e' || str=='i' || str=='o' || str=='u')
+  printf("vowel");
+  else
+   printf("consonant");
+   return 0;
+   }
