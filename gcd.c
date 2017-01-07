d9f6731bb2399064a099b78122db7b97ad172b66
MuthuA <kuttymuthu55@gmail.com>
2017-01-07 05:25:46 -0800

    Create gcd.c

---
+++ gcd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+ int n,n1,i,gcd;
+printf("enter two values:");
+scanf("%d%d",&n,&n1);
+for(i=1;i<=n1&&i<=n2;i++)
+{
+ if(i%n==0 && i%n1==0)
+  gcd=i;
+ }
+ printf("%d",gcd);
+ return 0;
+}
