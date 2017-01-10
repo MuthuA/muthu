bf0bc0dacbbaaa938583216a745e11bb5840e0bc
MuthuA <kuttymuthu55@gmail.com>
2017-01-09 19:15:57 -0800

    Create countline.c

---
+++ countline.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+ char str[50];
+ int i,len=0,count=0;
+ printf("enter string");
+ gets(str);
+ for(i=0;str[i]!='\0';i++)
+ {
+  len++;
+ }
+ for(i=0;i<len;i++)
+ {
+  if(str[i]=='.')
+  {
+   count++;
+  }
+ }
+ printf("%d",count);
+ return 0;
+ }
