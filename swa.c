aaef2fbd630e2a14c12870c2bcd9fd040103e3ae
MuthuA <kuttymuthu55@gmail.com>
2017-02-22 09:26:54 -0800

    Update swa.c

--- swa.c
+++ swa.c
@@ -1,17 +1,26 @@
-#include <stdio.h>
-#include <string.h>
-void main(){
-     char str[20],tmp;
-     int i,j;
-   
-     printf("\nEnter a string : ");
-     scanf("%s",str);
-     printf("\n\nOriginal String     : %s",str);
-     for(i=0;i<strlen(str);i=i+2){
-     tmp = str[i];
-     str[i] = str[i+1];
-     str[i+1] = tmp;
-     }
-     printf("\nAfter Swap String      : %s",str);
+import java.util.*;
+class Sample
+{
+    public static void main(String args[])
+    {
+        String input;
+        StringBuilder output = new StringBuilder();
+        Scanner sc=new Scanner(System.in);
+        input=sc.nextLine();
 
+        char[] characters = input.toCharArray();
+
+       for (int i = 0; i < characters.length; i++)
+     {
+      if (i % 2 == 0)
+      {
+    if((i+1) < characters.length)
+    {
+      output.append(characters[i + 1]);
+    }
+               output.append(characters[i]);
+  }
+}
+System.out.println(output);
+}
 }
