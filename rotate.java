477e902a2bdcb9033b4eb94b7ac9dfcb11e402ce
MuthuA <kuttymuthu55@gmail.com>
2017-02-23 06:11:11 -0800

    Create rotate.java

---
+++ rotate.java
@@ -0,0 +1,26 @@
+public class Pro
+{
+    public static void main(String args[])
+    {
+        int i,j,n=7,k=3;
+        int a[]={1,2,3,4,5,6,7};
+        int m=0,r;
+        j=n-k;
+        r=0;
+        while(m<n)
+        {
+            if(m<k)
+            {
+                System.out.print(a[j]);
+                j++;
+            }
+                else
+            {
+                System.out.print(a[r]);
+                r++;
+        
+         }
+            m++;
+}
+}
+}
